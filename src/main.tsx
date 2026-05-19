f51a2ac59a760ab290f123346813a7f5a2586d6b
tsukiisjewels-cell <tsukiisjewels@gmail.com>
2026-05-19 00:14:05 -0400

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
 import { MobileApp } from './app/MobileApp';
 import './styles.css';
 
-const isMobileApp =
-  typeof window !== 'undefined' &&
-  (window.location.pathname === '/app' || window.location.pathname.startsWith('/app/'));
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>{isMobileApp ? <MobileApp /> : <App />}</React.StrictMode>,
+  <React.StrictMode>
+    <MobileApp />
+  </React.StrictMode>,
 );
